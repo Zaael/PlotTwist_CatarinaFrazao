3e8ea97a0b0051588b097cd535604ff43ade6829
Zaael <israelsouto.s@gmail.com>
2019-10-25 20:20:52 -0300

    alterações no template e na estrutura do projeto

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,18 +1,31 @@
 from app import app
 import mysql.connector
-
-from mysql.connector.errors import Error
-from flask import render_template, request
 from app.services import db
-from app.controllers import login
-from app.controllers import logout
-from app.controllers import home
+from mysql.connector.errors import Error
+from flask import render_template, request, redirect, url_for, session
 
 connection = db.db_connection()
 
-@app.route('/cadastro_produto', methods=['GET', 'POST'])
-def cadastro_produto():
-    return render_template('cadastro-produtos.html')
+@app.route('/produtos')
+def produto():
+    if 'loggedin' in session:
+        return render_template('produtos.html',
+                               username=session['username'],
+                               loggedin=session['loggedin'],
+                               breadcrumb='Produtos',
+                               page_header='Menu de Navegação')
+    return redirect(url_for('login'))
+
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def redirecionarProduto():
+    if 'loggedin' in session:
+        return render_template('cadastro-produtos.html',
+                                username=session['username'],
+                                loggedin=session['loggedin'],
+                                breadcrumb='Cadastro Produtos',
+                                page_header='Menu de Cadastro')
+    return redirect(url_for('login'))
 
 
 @app.route('/criar_produto', methods=['POST', 'GET'])
@@ -25,12 +38,10 @@ def criar_produto():
             cursor.execute('INSERT INTO Produtos (Descricao, Marca) VALUES (%s, %s)', (descProduto, marcaProduto))
             connection.commit()
             msg = 'Cadastro realizado com sucesso!'
-            return render_template('cadastro-produtos.html', msg=msg)
+            return redirect(url_for('redirecionar'))
         except mysql.connector.Error as err:
             msg = 'Ops! Algo deu errado. Verifique as informações e tente novamente. Erro: {}'.format(err)
-            return render_template('cadastro-produtos.html', msg=msg)
-
-# http://localhost:5000/python/logout - this will be the logout page
+            return redirect(url_for('redirecionar'))
 
 
 
--- app/controllers/routesFornecedor.py
+++ app/controllers/routesFornecedor.py
@@ -10,7 +10,17 @@ from app.controllers import home
 
 connection = db.db_connection()
 
-@app.route('/cadastro_fornecedor/', methods=['GET', 'POST'])
+@app.route('/fornecedor/')
+def fornecedor():
+    if 'loggedin' in session:
+        return render_template('fornecedor.html',
+                                username=session['username'],
+                                loggedin=session['loggedin'],
+                                breadcrumb='Fornecedor',
+                                page_header ='Menu de Navegação')
+    return redirect(url_for('login'))
+
+@app.route('/cadastro_fornecedor', methods=['GET', 'POST'])
 def cadastro_fornecedor():
     if 'loggedin' in session:
         return render_template('cadastro-fornecedor.html',
@@ -23,7 +33,7 @@ def cadastro_fornecedor():
 
 @app.route('/criar_fornecedor', methods=['GET','POST'])
 def criar_fornecedor():
-    if request.method == 'POST':
+    if  request.method == 'POST':
         nomeFornecedor = request.form['Nome_Fornecedor']
         cnpj = request.form['CNPJ']
         contato = request.form['Contato']
@@ -31,8 +41,51 @@ def criar_fornecedor():
             cursor = connection.cursor()
             cursor.execute('INSERT INTO Fornecedor (nome_Fornecedor, CNPJ, Contato) VALUES (%s, %s, %s)',(nomeFornecedor,cnpj,contato))
             connection.commit()
-            msg = 'Cadastro de Fornecedor realizado com sucesso!'
+            #msg = 'Cadastro de Fornecedor realizado com sucesso!'
             return redirect(url_for('cadastro_fornecedor'))
         except mysql.connector.Error as err:
             msg = 'Ops! Algo deu errado. Verifique as informações e tente novamente. Erro: {}'.format(err)
-            return render_template('cadastro-fornecedor.html',msg=msg)
+            return redirect(url_for('cadastro_fornecedor'))
+
+@app.route('/buscar',methods=['GET','POST'])
+def buscar():
+    if 'loggedin' in session:
+        return render_template('buscar_fornecedor.html',
+                                username=session['username'],
+                                loggedin=session['loggedin'],
+                                breadcrumb='Consultar Fornecedores',
+                                page_header='Menu de Consulta')
+    return redirect(url_for('login'))
+
+@app.route('/buscar_fornecedores', methods=['GET','POST'])
+def buscar_fornecedores():
+    msg = ''
+    data = []
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT Nome_Fornecedor, CNPJ, Contato from Fornecedor")
+        dadosFornecedor = cursor.fetchall()
+        print(dadosFornecedor)
+        data = [list(item) for item in dadosFornecedor]
+        dicionarioFornecedor = {'data':data}
+        print(dicionarioFornecedor)
+        return dicionarioFornecedor
+    except mysql.connector.Error as err:
+        msg ='Ops! Algo deu errado. Verifique as informações e tente novamente. Erro: {}'.format(err)
+        return render_template('fornecedor.html',msg = msg)
+
+
+@app.route('/alterarFornecedor/',methods=['GET','POST'])
+def AlteraFornecedor():
+    if 'loggedin' in session:
+        
+        return render_template('alterarfornecedor.html')
+
+
+def ListarFornecedores():
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Fornecedor")
+    fornecedores = cursor.fetchall()
+    print (fornecedores[0])
+    print (type(fornecedores))
+    return fornecedores
