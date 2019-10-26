b987116a15466fb6f2e6141092766665d14342d5
kayke2000 <bentokayke@gmail.com>
2019-10-25 21:15:48 -0300

    datatableeditor

--- app/controllers/clienteAlterar.py
+++ app/controllers/clienteAlterar.py
@@ -3,21 +3,8 @@ import mysql.connector
 from app.services import db
 from mysql.connector.errors import Error
 from flask import render_template, request, redirect, url_for, session
-import json
 
 connection = db.db_connection()
-
-
-@app.route('/consulta', methods=['GET', 'POST'])
-def redirecionarConsulta(mensagem = 'teste'):
-    msg = mensagem
-    if 'loggedin' in session:
-        return render_template('clientes-consulta.html',
-                                username=session['username'],
-                                loggedin=session['loggedin'],
-                                breadcrumb='Consultar Clientes',
-                                page_header='Menu de Consulta')
-    return redirect(url_for('login'))
 
 @app.route('/consultar_cliente', methods=['GET','POST'])
 def consultar_cliente():
@@ -26,7 +13,7 @@ def consultar_cliente():
 
     try:
         cursor = connection.cursor()
-        cursor.execute('SELECT Nome, CPF, Celular, Email from cliente ')
+        cursor.execute('UPDATE cliente SET Nome =  CPF, Celular, Email  ')
         dadosCliente = cursor.fetchall()
         data = [list(item) for item in dadosCliente]
         dicDados = {'data':data}
@@ -35,4 +22,3 @@ def consultar_cliente():
     except mysql.connector.Error as err:
         msg = 'Ops! Algo deu errado. Erro: {0}'.format(err)
         return redirecionar(msg)
-
